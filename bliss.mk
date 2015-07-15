ee3fb72c7264f72558747f8e7db32aab5c226fb6
electrikjesus <electrikjesus@gmail.com>
2015-07-15 10:21:58 -0400

    Blissified the makefile

--- bliss.mk
+++ bliss.mk
@@ -1,10 +1,10 @@
 $(call inherit-product, device/samsung/jfltespr/full_jfltespr.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/bliss/config/nfc_enhanced.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common Bliss stuff.
+$(call inherit-product, vendor/bliss/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=jfltespr \
@@ -14,5 +14,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
 
-PRODUCT_NAME := cm_jfltespr
+PRODUCT_NAME := bliss_jfltespr
 PRODUCT_DEVICE := jfltespr
